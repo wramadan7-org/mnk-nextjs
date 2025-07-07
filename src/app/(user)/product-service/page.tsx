355384214262d70b157cf4086b2bc1272d7b1172
Wahyu Ramadan <wramadan1203@gmail.com>
2025-07-07 12:27:53 +0700

    chore: initial product service

---
+++ src/app/(user)/product-service/page.tsx
@@ -0,0 +1,11 @@
+import HeroComponent from "@/components/HeroComponent";
+
+export default function ProductService() {
+  return (
+    <main className="flex flex-col items-center justify-start min-h-screen bg-gray-100 overflow-x-hidden">
+      <HeroComponent />
+
+      <h1>Product & Service</h1>
+    </main>
+  );
+}
